998f56d1e581994f70e368144db26f416778b7bb
Christopher Zahumensky <zahumensky.krystof@gmail.com>
2021-05-07 23:54:02 +0200

    Somewhat functioning app

--- src/components/WriteOffTable.vue
+++ src/components/WriteOffTable.vue
@@ -1,25 +1,40 @@
 <template>
-  <div class="writeoff-table">
-    <h3>Název</h3>
-    <table>
-      <thead>
-      <th>Rok</th>
-      <th>Zůstatková cena</th>
-      <th>Roční odpis</th>
-      <th>Odpis celkem</th>
-      </thead>
-      <tbody>
-      <tr>
-        <td></td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+  <transition>
+    <div v-if="show" class="writeoff-table">
+      <h3>{{ writeOffName }}</h3>
+      <table>
+        <tr>
+          <th>Rok</th>
+          <th>Zůstatková cena</th>
+          <th>Roční odpis</th>
+          <th>Odpis celkem</th>
+        </tr>
+        <tr v-for="writeOff in data" :key="writeOff.year">
+          <td>{{ writeOff.year }}</td>
+          <td>{{ writeOff.restPrice }} Kč</td>
+          <td>{{ writeOff.yearly }} Kč</td>
+          <td>{{ writeOff.total }} Kč</td>
+        </tr>
+      </table>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
-  name: "WriteOffTable"
+  name: "WriteOffTable",
+  props: {
+    data: {
+      type: Array
+    },
+    show: {
+      type: Boolean,
+      immediate: true
+    },
+    writeOffName: {
+      type: String
+    }
+  }
 }
 </script>
 
--- src/views/WriteOff.vue
+++ src/views/WriteOff.vue
@@ -3,19 +3,19 @@
     <div class="form">
       <div class="form-group">
         <label for="name">Název</label>
-        <input type="text" id="name">
+        <input type="text" v-model="writeOffName" id="name">
       </div>
       <div class="form-group">
         <label for="price">Cena</label>
-        <input type="text" id="price">
+        <input type="text" v-model="price" id="price">
       </div>
       <div class="form-group">
         <label for="date">Rok pořízení</label>
-        <input type="number" id="date">
+        <input type="number" v-model="year" id="date">
       </div>
       <div class="form-group">
         <label for="group">Odpisová skupina</label>
-        <select id="group">
+        <select id="group" v-model="group">
           <option value="1">1 (3 roky)</option>
           <option value="2">2 (5 let)</option>
           <option value="3">3 (10 let)</option>
@@ -25,11 +25,12 @@
         </select>
       </div>
       <div class="checkbox">
-        <input id="fastWriteOff" type="checkbox">
+        <input id="fastWriteOff" v-model="fastWriteOff" type="checkbox">
         <label for="fastWriteOff">Zrychlený odpis</label>
       </div>
+      <button v-on:click="calculateWriteOff()">Vypočítej</button>
     </div>
-    <write-off-table></write-off-table>
+    <write-off-table :data="writeOffs" :show="showTable" :name="writeOffName"></write-off-table>
   </div>
 </template>
 
@@ -40,8 +41,103 @@ export default {
   components: {
     WriteOffTable
   },
+  data() {
+    return{
+      price: 0,
+      group: 1,
+      writeOffName: "",
+      year: 2021,
+      fastWriteOff: false,
+      writeOffs: [],
+      showTable: false
+    }
+  },
   methods: {
+    calculateWriteOff: function (){
+      console.log(this.price)
+      console.log(this.group)
+      console.log(this.writeOffName)
+      console.log(this.year)
 
+      this.writeOffs = []
+      let firstYearC = 0
+      let nextYearsC = 0
+      let firstYear = 0
+      let nextYears = 0
+      let yearsCount = 0
+      let rest = this.price
+      let total = 0
+      if(this.group === 1){
+        firstYear = 20
+        nextYears = 40
+        firstYearC = 3
+        nextYearsC = 4
+        yearsCount = 3
+      }
+      if(this.group === 2){
+        firstYear = 11
+        nextYears = 22.25
+        firstYearC = 5
+        nextYearsC = 6
+        yearsCount = 5
+      }
+      if(this.group === 3){
+        firstYear = 5.5
+        nextYears = 10.5
+        firstYearC = 10
+        nextYearsC = 11
+        yearsCount = 10
+      }
+      if(this.group === 4){
+        firstYear = 2.15
+        nextYears = 5.15
+        firstYearC = 20
+        nextYearsC = 21
+        yearsCount = 20
+      }
+      if(this.group === 5){
+        firstYear = 1.4
+        nextYears = 3.4
+        firstYearC = 30
+        nextYearsC = 31
+        yearsCount = 30
+      }
+      if(this.group === 6){
+        firstYear = 1.02
+        nextYears = 2.02
+        firstYearC = 50
+        nextYearsC = 51
+        yearsCount = 50
+      }
+
+      if(!this.fastWriteOff){
+        for (let i = 0; i < yearsCount; i++){
+          let yearly = 0
+          if(i === 0){
+            yearly = (this.price / 100) * firstYear
+          }
+          else yearly = (this.price / 100) * nextYears
+
+          rest = rest - yearly
+          total += yearly
+          this.writeOffs.push({year: this.year + i, restPrice: rest, yearly: yearly, total: total})
+        }
+      } else {
+        for (let i = 0; i < yearsCount; i++){
+          let yearly = 0
+          if(i === 0){
+            yearly = this.price / firstYearC
+          }
+          else yearly = (2*rest) / (nextYearsC - (i+1))
+
+          rest = rest - yearly
+          total += yearly
+          this.writeOffs.push({year: this.year + i, restPrice: rest, yearly: yearly, total: total})
+        }
+      }
+      this.showTable = true
+      console.log(this.showTable)
+    }
   }
 }
 </script>
